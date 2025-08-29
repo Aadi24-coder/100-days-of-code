b91c99cfc06796bd484f782975b7e2fbb3c69157
Aadi Agarwal <136228683+Aadi24-coder@users.noreply.github.com>
2025-08-29 11:37:06 +0530

    Create sum.c
    find the sum of two numbers

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+   int a,b;
+   int sum=0;
+   printf(" enter two numbers ");
+   scanf("%d%d ", &a,&b);
+   sum=a+b;
+   printf(" sum of two numbers is %d\n" , sum);
+   return 0;
+}
