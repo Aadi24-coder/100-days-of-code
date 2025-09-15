40359ab871fc6944bdfeb8a068f0f654502f74db
Aadi Agarwal <136228683+Aadi24-coder@users.noreply.github.com>
2025-09-15 20:10:16 +0530

    pattern
    create a pattern

---
+++ pattern4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=5;i=i+2)
+    {
+        for(int j=1;j<=i;j++)
+    {
+        printf("*\n");
+
+    }
+    printf("\n");
+    }
+    for(int i =3;i>0;i=i-2)
+    {
+        for(int j =1;j<=i;j++)
+        {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+     return 0;
+}
